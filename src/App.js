cbb24a0c6b1cf9464f79d8665013c5cf3446b255
Semir Cohovic <semircohovic@gmail.com>
2020-08-05 18:59:13 +0200

    adding sign up

--- src/App.js
+++ src/App.js
@@ -1,36 +1,109 @@
-import React, {useState} from 'react';
+import React, { useState, useEffect } from 'react';
 import Post from './Post';
 import './App.css';
+import { db } from './firebase';
+
+import { makeStyles } from '@material-ui/core/styles';
+import Modal from '@material-ui/core/Modal';
+import { Button, Input } from '@material-ui/core';
+
+function rand() {
+  return Math.round(Math.random() * 20) - 10;
+}
+
+function getModalStyle() {
+  const top = 50 + rand();
+  const left = 50 + rand();
+
+  return {
+    top: `${top}%`,
+    left: `${left}%`,
+    transform: `translate(-${top}%, -${left}%)`,
+  };
+}
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    position: 'absolute',
+    width: 400,
+    backgroundColor: theme.palette.background.paper,
+    border: '2px solid #000',
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing(2, 4, 3),
+  },
+}));
 
 function App() {
-  const [posts, setPosts] = useState([
-    {
-      username: 'semircohovic',
-      caption: "Wow it works",
-      imageUrl: "https://fikrabd.com/sites/default/files/web-development.jpg"
+  const classes = useStyles();
+  const [modalStyle] = React.useState(getModalStyle);
 
-    }, {
-      username :"mersiha",
-      caption: "Chilling",
-      imageUrl :"https://thumbs.dreamstime.com/b/boy-watching-movie-home-boy-watching-movie-home-illustration-relaxed-man-sitting-big-room-front-modern-television-153246274.jpg"
-    }, {
-      username: "milovan",
-      caption: "Lezanija",
-      imageUrl: "https://www.wallpaperflare.com/static/552/968/15/women-brunette-closed-eyes-face-wallpaper.jpg"
-    }
-  ]);
+  const [posts, setPosts] = useState([]);
+  const [open, setOpen] = useState(false);
+  const [username, setUsername] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  useEffect(() => {
+    db.collection('posts').onSnapshot(snapshot => {
+      setPosts(snapshot.docs.map(doc => ({
+        id: doc.id,
+        post: doc.data()
+      })));
+    });
+  }, []);
+  const signUp = (event) => {
+
+  }
 
   return (
     <div className="app">
+      <Modal
+        open={open}
+        onClose={() => setOpen(false)}
+      >
+        <div style={modalStyle} className={classes.paper}>
+          <form>
+            <center>
+              <img
+                className="app__headerImage"
+                src="https://www.instagram.com/static/images/web/"
+                alt=""
+              />
+            </center>
+            <Input
+              type="text"
+              placeholder="username"
+              vlaue={username}
+              onChange={(e) => setUsername(e.target.vlaue)}
+            />
+            <Input
+              type="text"
+              placeholder="email"
+              vlaue={username}
+              onChange={(e) => setEmail(e.target.vlaue)}
+            />
+            <Input
+              type="password"
+              placeholder="password"
+              vlaue={username}
+              onChange={(e) => setPassword(e.target.vlaue)}
+            />
+            <Button onClick={signUp}>Sign Up</Button>
+          </form>
+        </div>
+      </Modal>
       <div className="app__header">
-        <img 
+        <img
           className="app__headerImage"
           src="https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png"
         />
       </div>
+      <Button onClick={() => setOpen(true)}>Sign Up</Button>
       {
-        posts.map(post => (
-          <Post username={post.username} caption={post.caption} imageUrl={post.imageUrl} />
+        // odje je vazno ako dodamo id on ce samo da doda novi post nece da refresuje ostale a 
+        // ako ostavimo da je bez id tj key on ce stalno da refresuje ostale postoce koje smo loopvali
+        posts.map(({ post, id }) => (
+          <Post key={id} username={post.username} caption={post.caption} imageUrl={post.imageUrl} />
         ))
       }
     </div>
--- src/Post.js
+++ src/Post.js
@@ -1,21 +1,21 @@
 import React from 'react'
 import './Post.css';
 import Avatar from '@material-ui/core/Avatar';
-function Post({ username, caption, imageUrl}) {
+function Post({ username, caption, imageUrl }) {
     return (
         <div className="post">
             <div className="post__header">
-            <Avatar 
-                className="post__avatar"
-                alt="Semir Cohovic"
-                src="/static/images/avatar/1.png"
-            />
-            <h2>{username}</h2>
+                <Avatar
+                    className="post__avatar"
+                    alt="Semir Cohovic"
+                    src="/static/images/avatar/1.png"
+                />
+                <h2>{username}</h2>
             </div>
             <img className="post__image" src={imageUrl} />
-     
+
             <h4 className="post__text"><strong>{username}</strong> {caption}</h4>
-            
+
         </div>
     )
 }
