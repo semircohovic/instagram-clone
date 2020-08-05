cae976bf6dfa6169655887c2f3df9b6fa5fed05f
Semir Cohovic <semircohovic@gmail.com>
2020-08-05 16:08:32 +0200

    adding firebase

--- src/App.js
+++ src/App.js
@@ -1,17 +1,38 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Post from './Post';
 import './App.css';
 
 function App() {
+  const [posts, setPosts] = useState([
+    {
+      username: 'semircohovic',
+      caption: "Wow it works",
+      imageUrl: "https://fikrabd.com/sites/default/files/web-development.jpg"
+
+    }, {
+      username :"mersiha",
+      caption: "Chilling",
+      imageUrl :"https://thumbs.dreamstime.com/b/boy-watching-movie-home-boy-watching-movie-home-illustration-relaxed-man-sitting-big-room-front-modern-television-153246274.jpg"
+    }, {
+      username: "milovan",
+      caption: "Lezanija",
+      imageUrl: "https://www.wallpaperflare.com/static/552/968/15/women-brunette-closed-eyes-face-wallpaper.jpg"
+    }
+  ]);
+
   return (
-    <div className="App">
+    <div className="app">
       <div className="app__header">
         <img 
           className="app__headerImage"
           src="https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png"
         />
       </div>
-      <Post />
+      {
+        posts.map(post => (
+          <Post username={post.username} caption={post.caption} imageUrl={post.imageUrl} />
+        ))
+      }
     </div>
   );
 }
--- src/Post.js
+++ src/Post.js
@@ -1,13 +1,20 @@
 import React from 'react'
 import './Post.css';
-function Post() {
+import Avatar from '@material-ui/core/Avatar';
+function Post({ username, caption, imageUrl}) {
     return (
-        <div>
-            <h2>Username</h2>
-          
-            <img className="post__image" src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fprobella.com%2Fblog%2Fhow-to-make-react-javascript-seo-friendly%2F&psig=AOvVaw05oulZJWaguZxA_ph1iVU9&ust=1596718997829000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCKjrxPePhOsCFQAAAAAdAAAAABAD" />
+        <div className="post">
+            <div className="post__header">
+            <Avatar 
+                className="post__avatar"
+                alt="Semir Cohovic"
+                src="/static/images/avatar/1.png"
+            />
+            <h2>{username}</h2>
+            </div>
+            <img className="post__image" src={imageUrl} />
      
-            <h4 className="post__text"><strong>semircohovic</strong> React js learning!!</h4>
+            <h4 className="post__text"><strong>{username}</strong> {caption}</h4>
             
         </div>
     )
